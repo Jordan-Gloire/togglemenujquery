43f741b84a2f3407f0991c3a2008f8ad859be2dc
Jordan Gloire <132207668+Jordan-Gloire@users.noreply.github.com>
2024-02-11 01:20:11 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    animation: {
+        'bounce-slow': 'bounce 2s linear infinite',
+      },
+    extend: {},
+  },
+  plugins: [],
+}
+
